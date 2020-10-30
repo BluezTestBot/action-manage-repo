956479a1716004f979a7b829df97067105e9ed57
Tedd Ho-Jeong An <tedd.an@intel.com>
2020-10-30 09:21:05 -0700

    Add support for bluetooth-next tree
    This patch adds an input parameter for for_upstream branch which is used
    to distinguish between bluez and bluetooth-next tree.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
+if [ -z "$GITHUB_TOKEN" ]; then
     echo "Set GITHUB_TOKEN environment variable"
     exit 1
 fi
 
-/manage_repo.sh "$@"
+if [ "$FOR_UPSTREAM_BRANCH" = 'none' ]; then
+    /manage_repo.sh "$@"
+else
+    /manage_repo_for_upstream.sh "$@"
+fi
